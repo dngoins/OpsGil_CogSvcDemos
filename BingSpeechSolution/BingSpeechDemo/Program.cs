c99227f9b5167eca058c3255711249e5b2ff80b1
Dwight Goins <dngoins@hotmail.com>
2018-01-22 06:34:34 +0000

    Added BingSpeech Demo

---
+++ BingSpeechSolution/BingSpeechDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace BingSpeechDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bing Speech Hello World!");
+
+            var requestUri = "https://speech.platform.bing.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US&format=detailed";
+
+            var AUDIO_FILE = @".\Assets\This Is A funny Story.wav";
+
+            var SUBSCRIPTION_KEY = "f62b5082470e4977bda8115e8fd35738";
+
+            var responseString = "";
+
+            HttpWebRequest request = null;
+            request = (HttpWebRequest)HttpWebRequest.Create(requestUri);
+            request.SendChunked = true;
+            request.Accept = @"application/json;text/xml";
+            request.Method = "POST";
+            request.ProtocolVersion = HttpVersion.Version11;
+            request.ContentType = @"audio/wav; codec=audio/pcm; samplerate=16000";
+            request.Headers["Ocp-Apim-Subscription-Key"] =  SUBSCRIPTION_KEY;
+
+
+            // Send an audio file by 1024 byte chunks
+            using (var fs = new FileStream(AUDIO_FILE, FileMode.Open, FileAccess.Read))
+            {
+
+                /*
+                * Open a request stream and write 1024 byte chunks in the stream one at a time.
+                */
+                byte[] buffer = null;
+                int bytesRead = 0;
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    /*
+                    * Read 1024 raw bytes from the input audio file.
+                    */
+                    buffer = new Byte[checked((uint)Math.Min(1024, (int)fs.Length))];
+                    while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        requestStream.Write(buffer, 0, bytesRead);
+                    }
+
+                    // Flush
+                    requestStream.Flush();
+                }
+            }
+
+            Console.WriteLine("Response:");
+            using (WebResponse response = request.GetResponse())
+            {
+                Console.WriteLine(((HttpWebResponse)response).StatusCode);
+
+                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                {
+                    responseString = sr.ReadToEnd();
+                }
+
+                Console.WriteLine(responseString);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
